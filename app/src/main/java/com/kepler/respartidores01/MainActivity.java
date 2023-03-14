b31678d985cb89426bca2e8196a0015181fc8647
Sofia-jimenez <fofi.bibi04@gmail.com>
2023-03-13 23:26:12 -0600

    subi imagenes y nombres

--- app/src/main/java/com/kepler/respartidores01/MainActivity.java
+++ app/src/main/java/com/kepler/respartidores01/MainActivity.java
@@ -1,8 +1,14 @@
 package com.kepler.respartidores01;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public static class spinner extends AppCompatActivity {
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            Spinner sempresas= findViewById(R.id.spiner1);
+            ArrayList<String> lista = new ArrayList<String>();
+            lista.add("Autodis");
+            lista.add("Vipla");
+            lista.add("Cecra");
+
+           ArrayAdapter<String> spinerArray = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, lista);
+           spinerArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+           sempresas.setAdapter(spinerArray);
+
+
+
+        }
+    }
 }
+
