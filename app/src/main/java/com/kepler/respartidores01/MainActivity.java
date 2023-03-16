214231f558ddc3ee932b27188358ce6bdda4c4ad
Sofia-jimenez <fofi.bibi04@gmail.com>
2023-03-15 22:25:07 -0600

    15/03/23

--- app/src/main/java/com/kepler/respartidores01/MainActivity.java
+++ app/src/main/java/com/kepler/respartidores01/MainActivity.java
@@ -1,48 +1,104 @@
 package com.kepler.respartidores01;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
     private Spinner sempresas;
     private String[] lista;
     private ArrayAdapter<String> adapter;
     private ImageView foto;
-    int posici;
-    private boolean bandera = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner();
-        fotos();
 
     }
 
     private void spinner() {
         sempresas = findViewById(R.id.spinerempresas);
+        foto=findViewById(R.id.imageView1);
         lista = new String[]{"Autodis","Vipla","Jacve", "Cecra", "Guvi", "Pressa"};
         adapter= new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, lista);
         sempresas.setAdapter(adapter);
 
+        sempresas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String selection = (String) adapterView.getItemAtPosition(i);
+
+                if(selection=="Autodis"){
+                    foto.setImageResource(R.drawable.autodis);
+                }else if (selection=="Vipla"){
+                    foto.setImageResource(R.drawable.vipla);
+                } else if (selection=="Jacve") {
+
+
+                } else if (selection=="Cecra") {
+
+                } else if (selection=="Guvi") {
+
+                }else{
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+    public void sendMessage(View view){
+         sesion();
     }
 
-    private void fotos(){
+    public void sesion(){
+        EditText textusu = findViewById(R.id.txtinUsu);
+        String usua = textusu.getText().toString();
+        EditText textcont =findViewById(R.id.txtinCla);
+        String conta= textcont.getText().toString();
+
+        if(usua.equals("admin") && conta.equals("admin")){
+
+                Intent inteto= new Intent(this, Principal.class);
+                startActivity(inteto);
+
+        }else {
+            Toast tost= Toast.makeText(this, "usuaio o contraseña incorrecta", Toast.LENGTH_LONG);
+            tost.show();
+        }
+
+
 
     }
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+
         }
+
 
     }
 
---
+++ app/src/main/java/com/kepler/respartidores01/Principal.java
@@ -0,0 +1,66 @@
+package com.kepler.respartidores01;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kepler.respartidores01.databinding.ActivityPrincipalBinding;
+
+public class Principal extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityPrincipalBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarPrincipal.toolbar);
+        binding.appBarPrincipal.toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_principal);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+}
